82ca28f0550bd3bdae0b2c051ba87162787d7339
Nils <nilsl@student.ethz.ch>
2016-10-25 15:34:28 +0200

    Layout fixes + Gradle live demo resources

---
+++ resources/gradle/src/Hai.java
@@ -0,0 +1,6 @@
+public class Hai {
+	public Hai() {
+		System.out.println("Hi from class Hai!");
+		System.out.println("How's it going?");
+	}
+}
---
+++ resources/gradle/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hey World, it's me!");
+		Hai hai = new Hai();
+	}
+}
